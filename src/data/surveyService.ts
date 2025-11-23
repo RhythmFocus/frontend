84421a74cb14eae49f9174459151e1752f14acf5
cbnuLeehyunwoo <henow123@naver.com>
2025-11-23 12:08:50 +0900

    refactor: cfq의 설문 데이터 및 간단한 메타정보만 src/data에서 관리하도록 변경

--- src/data/surveyService.ts
+++ src/data/surveyService.ts
@@ -1,6 +1,7 @@
 import { SurveyConfig } from '../types/survey.types';
 import { ASRS_DATA } from './asrs';
 import { SNAP_IV_DATA } from './snap_iv';
+import { CFQ_DATA } from './cfq'; // Add this import
 
 // ID를 받아서 해당 데이터를 리턴하는 헬퍼 함수
 export const getSurveyDataById = (id: string): SurveyConfig | null => {
@@ -9,6 +10,8 @@ export const getSurveyDataById = (id: string): SurveyConfig | null => {
             return ASRS_DATA;
         case 'SNAP_IV':
             return SNAP_IV_DATA;
+        case 'CFQ': // Add this case
+            return CFQ_DATA;
         default:
             return null;
     }
